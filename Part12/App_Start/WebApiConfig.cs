823a1370cecd445d4fa3e608acc6194926632701
Denham Dagger <denhamdagger@gmail.com>
2018-02-09 15:43:28 +0000

    Part11 added

--- Part12/App_Start/WebApiConfig.cs
+++ Part12/App_Start/WebApiConfig.cs
@@ -1,24 +1,55 @@
-﻿using System;
+﻿using Microsoft.OData;
+using Microsoft.OData.Edm;
+using Microsoft.OData.UriParser;
+using Part12.Models;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web.Http;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+using System.Web.OData.Routing.Conventions;
 
 namespace Part12
 {
-    public static class WebApiConfig
-    {
-        public static void Register(HttpConfiguration config)
-        {
-            // Web API configuration and services
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			// OData Routing
+			var model = GetEdmModel();
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
+			config.MapODataServiceRoute("OData", "OData", b => b
+				.AddService<IEdmModel>(ServiceLifetime.Singleton, s => model)
+				.AddService<IEnumerable<IODataRoutingConvention>>(ServiceLifetime.Singleton, s => ODataRoutingConventions.CreateDefaultWithAttributeRouting("OData", config))
+				.AddService<ODataUriResolver>(ServiceLifetime.Singleton, s => new AlternateKeysODataUriResolver(model))
+			);
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-        }
-    }
+		}
+
+		private static IEdmModel GetEdmModel()
+		{
+			EdmModel _builder = new EdmModel();
+
+			// Create Car Entity
+			EdmEntityType car = new EdmEntityType("Part12.Models", "Car");
+			car.AddKeys(car.AddStructuralProperty("Make", EdmPrimitiveTypeKind.String), car.AddStructuralProperty("Model", EdmPrimitiveTypeKind.String));
+			car.AddStructuralProperty("Colour", EdmPrimitiveTypeKind.String);
+			car.AddStructuralProperty("Price", EdmPrimitiveTypeKind.Double);
+			var cat = car.AddStructuralProperty("Category", EdmPrimitiveTypeKind.String, false);
+			_builder.AddAlternateKeyAnnotation(car, new Dictionary<string, IEdmProperty>
+			{
+				{"Category", cat}
+			});
+			_builder.AddElement(car);
+
+			// Create Container
+			EdmEntityContainer container = new EdmEntityContainer("default", "Container");
+
+			// Add the Cars Entity into the Container
+			EdmEntitySet cars = container.AddEntitySet("Cars", car);
+			_builder.AddElement(container);
+
+			return _builder;
+		}
+	}
 }
+
---
+++ Part12/Controllers/CarsController.cs
@@ -0,0 +1,59 @@
+﻿using Part12.DataSource;
+using Part12.Models;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.OData;
+using System.Web.OData.Routing;
+
+namespace Part12.Controllers
+{
+	[ODataRoutePrefix("Cars")]
+	public class VehicleController : ODataController
+	{
+		private Repository _repo;
+
+		public VehicleController()
+		{
+			_repo = new Repository();
+		}
+
+		// OData/Cars
+		[EnableQuery]
+		[ODataRoute("")]
+		public IQueryable<Car> GetAllCars()
+		{
+			return _repo.GetCars();
+		}
+
+		// OData/Cars(Make='Vauxhall', Model='Zafira')
+		[EnableQuery]
+		[ODataRoute("(Make={KeyMake}, Model={KeyModel})")]
+		public Car Get([FromODataUri] string KeyMake, [FromODataUri] string KeyModel)
+		{
+			return _repo.GetCar(KeyMake, KeyModel);
+		}
+
+		// OData/Cars(Category='4x4')
+		[EnableQuery]
+		[ODataRoute("(Category={Category})")]
+		public IQueryable<Car> GetCarByCategory([FromODataUri]string Category)
+		{
+			return _repo.GetCars(Category);
+		}
+
+		// OData/Cars(Make='Vauxhall', Model='Zafira')/Price
+		[ODataRoute("(Make={KeyMake}, Model={KeyModel})/Price")]
+		[ODataRoute("(Make={KeyMake}, Model={KeyModel})/Price/$value")]
+		public IHttpActionResult GetCarPrice([FromODataUri] string KeyMake, [FromODataUri] string KeyModel)
+		{
+			var _car = _repo.GetCar(KeyMake, KeyModel);
+			if (_car == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(_car.Price);
+		}
+	}
+}
